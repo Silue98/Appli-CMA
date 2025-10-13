c8fd7d0d4ce285405a7ddc4210955ef3f2fbe3e4
konan-fabrice <kouameaimefabrice@gmail.com>
2025-10-13 10:51:17 +0000

    FnGeek

--- CMA-App/server/api/predications/index.get.ts
+++ CMA-App/server/api/predications/index.get.ts
@@ -2,7 +2,7 @@ import prisma from '~/server/utils/prisma'
 
 export default defineEventHandler(async () => {
   return await prisma.predication.findMany({
-    include: { predications: true, annonces: true },
-    orderBy: { dateCulte: 'desc' },
+        
+   
   })
 })
